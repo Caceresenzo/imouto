c153b401475db0c6a4b326d80715f334559e0c9b
Caceresenzo <caceresenzo1502@gmail.com>
2021-05-01 13:06:01 +0000

    feat(telegraf): Monitoring docker via its socket

--- telegraf.sh
+++ telegraf.sh
@@ -4,6 +4,7 @@ docker run -d \
   -v /storage/:/storage/:ro \
   -v /redundant/:/redundant/:ro \
   -v /redundant/internal/config/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   --label "com.centurylinklabs.watchtower.enable=false" \
   --restart unless-stopped \
   telegraf

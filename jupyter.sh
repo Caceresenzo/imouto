60560c1f2bfd72ae287e1d6fe5d983e5a8f73248
Caceresenzo <caceresenzo1502@gmail.com>
2021-11-19 00:42:22 +0000

    feat(jupyter): starting with root access and using correct gid

--- jupyter.sh
+++ jupyter.sh
@@ -2,10 +2,15 @@
 #DIR=/redundant/work/workspace-13f
 #DIR=/redundant/work/datacrunch/
 
-docker run -d --rm \
-  --name "jupyther-$PORT" \
+docker run -d \
+  --name "jupyter-$PORT" \
   -p $PORT:8888 \
   -e JUPYTER_ENABLE_LAB=yes \
+  -e UID=0 \
+  -u root \
+  -e NB_GID=1003 \
+  -e GRANT_SUDO=yes \
   -v "$DIR":/home/jovyan/work \
+  --memory=105g \
   --label "com.centurylinklabs.watchtower.enable=false" \
-  jupyter/datascience-notebook:python-3.8.6
+  jupyter/datascience-notebook:python-3.9.6
